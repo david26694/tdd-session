0fa3831793abc84255398ac0e9c73c8eadf86ddd
david26694 <david26694@gmail.com>
2020-12-26 12:22:19 +0100

    Parametrize test

--- test/test_fizzbuzz.py
+++ test/test_fizzbuzz.py
@@ -1,14 +1,10 @@
 from src.fizzbuzz import fizzbuzz
+import pytest 
 
+@pytest.mark.parametrize(
+    "input,output",
+    [(2, 2), (3, 'Fizz'), (5, 'Buzz'), (15, 'FizzBuzz'), (30, 'FizzBuzz')]
+    )
+def test_io(input, output):
+    assert fizzbuzz(input) == output
 
-def test_return_2():
-    assert fizzbuzz(2) == 2
-
-def test_return_3():
-    assert fizzbuzz(3) == "Fizz"
-
-def test_return_5():
-    assert fizzbuzz(5) == "Buzz"
-
-def test_return_15():
-    assert fizzbuzz(15) == "FizzBuzz"
